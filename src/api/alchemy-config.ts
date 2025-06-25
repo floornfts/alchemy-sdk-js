a476732dbe6e9544285007ff32a31f1bafcf13e3
Chafik Boukhalfa <chafik@floornfts.io>
2025-06-25 15:46:57 +0200

    feat: remove hot-shots

--- src/api/alchemy-config.ts
+++ src/api/alchemy-config.ts
@@ -1,5 +1,3 @@
-import { StatsD } from 'hot-shots';
-
 import { ConnectionInfo } from '@ethersproject/web';
 
 import { AlchemySettings, Network } from '../types/types';
@@ -53,11 +51,6 @@ export class AlchemyConfig {
   readonly requestTimeout?: number;
 
   /**
-   * Optional StatsD client to use for tracking metrics.
-   */
-  readonly statsD: StatsD | undefined;
-
-  /**
    * Dynamically imported provider instance.
    *
    * @internal
@@ -82,7 +75,6 @@ export class AlchemyConfig {
     this.batchRequests = config?.batchRequests || false;
     this.requestTimeout = config?.requestTimeout || DEFAULT_REQUEST_TIMEOUT;
     this.connectionInfoOverrides = config?.connectionInfoOverrides;
-    this.statsD = config?.statsD;
   }
 
   /**
